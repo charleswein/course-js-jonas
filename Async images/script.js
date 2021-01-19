bed8010a3dd29e40b7a7dc352868c5bdeeb24614
Yury Dunets <jasper.hale.dev@gmail.com>
2021-01-20 00:03:41 +0300

    add: parallel effect

--- Async images/script.js
+++ Async images/script.js
@@ -1,16 +1,16 @@
 const wait = function (seconds) {
-  return new Promise(function (res, rej) {
+  return new Promise(function (res) {
     setTimeout(res, seconds * 1000);
-  }).then(res => console.log(res));
+  });
 };
 
-const createImage = function (imgPath) {
+const createImage = function (imgPath, cl) {
   return new Promise(function (res, rej) {
     const newImg = document.createElement('img');
     newImg.src = imgPath;
     newImg.addEventListener('load', function () {
       document
-        .querySelector('.images')
+        .querySelector(cl)
         .insertAdjacentElement('beforeend', newImg);
       res(newImg);
     });
@@ -20,24 +20,31 @@ const createImage = function (imgPath) {
   });
 };
 
-let currentImg;
-createImage('img/img-1.jpg')
-  .then(img => {
-    currentImg = img;
-    console.log('Image 1 loaded!');
-    return wait(2);
-  })
-  .then(() => {
-    currentImg.style.display = 'none';
-    return createImage('img/img-2.jpg');
-  })
-  .then((img) => {
-    currentImg = img;
-    console.log('Image 1 loaded!');
-    return wait(2);
-  })
-  .then(() => {
-    currentImg.style.display = 'none';
-    return createImage('img/img-3.jpg');
-  })
-  .catch(err => console.error(err));
+const loadNPause = async() =>{
+  try {
+    let img = await createImage('img/img-1.jpg','.images1');
+    await wait(2)
+    img.style.display = 'none';
+
+    img = await createImage('img/img-2.jpg','.images1');
+    await wait(2)
+    img.style.display = 'none';
+
+    img = await createImage('img/img-3.jpg','.images1');
+  } catch(err) {
+    err => console.error(err)
+  }
+}
+loadNPause()
+
+
+const loadAll = async imgArr => {
+  try {
+    let imgs = imgArr.map(async img => await createImage(img, '.images2'));
+    const arrImg = await Promise.all(imgs);
+    arrImg.forEach(img => img.classList.add('parallel'))
+  } catch (err) {
+    err => console.error(err);
+  }
+};
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
